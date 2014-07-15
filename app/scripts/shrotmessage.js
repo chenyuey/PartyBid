c007b2130c42c1e9727ad308ad0a0bca232ee184
chenyuey <wodechangemail@gmail>
2014-07-15 10:41:33 +0800

    完成shrotmessage.js的重构

--- app/scripts/shrotmessage.js
+++ app/scripts/shrotmessage.js
@@ -3,43 +3,74 @@ function parseJson (json_message)
     var messages = ShortMessage.getShortMessages();
     //正在报名的活动
     var bmActivity = BMingActivity.bmactivity();
-
-    //有开始报名的活动
     //去掉报名信息中的空格
     var message_content = json_message.messages[0].message.replace(/\s/g,"");
     var message_name = message_content.substring(2,message_content.length);
     //如果本地没有存储报名信息，则没有任何活动开始
-    if(bmActivity.length == 0)
-    {
-        native_accessor.send_sms(json_message.messages[0].phone,'活动尚未开始！');
-    }
-    else if(bmActivity[0].isStart == false)
-    {
-        native_accessor.send_sms(json_message.messages[0].phone,'活动已经结束！');
-    }else if( message_content.search(/bm|BM|bM|Bm/i) == 0)
+    NOActivityStart(json_message,bmActivity);
+
+    ActivityHasEnd(json_message,bmActivity);
+
+    BMStartDeal(bmActivity,message_content,message_name,json_message,messages);
+
+}
+//报名开始处理
+function BMStartDeal(bmActivity,message_content,message_name,json_message,messages)
+{
+    if(bmActivity.length != 0 && bmActivity[0].isStart == true && message_content.search(/bm|BM|bM|Bm/i) == 0)
     {
         //存储在正在报名的活动
         var message = new ShortMessage(message_name,json_message.messages[0].phone,bmActivity[0].name);
-
-        if(firstBMSMS(json_message,messages,message) == false)
-        {
-            var isExist = false;
-            if(_.find(messages,function(message){
-                return message.phone == json_message.messages[0].phone && message.activity == bmActivity[0].name;
-            }))
-            {
-                isExist = true;
-            }
-
-            if(insert_sms_return_sms(json_message,message,isExist) == false)
-            {
-                native_accessor.send_sms(json_message.messages[0].phone,'已经报过名！');
-                console.log('已经报过名！');
-            }
-        }
+        BMSMSDeal(json_message,messages,message,bmActivity);
 
     }
-
+}
+//报名信息处理（包括第一个报名与非第一个报名）
+function BMSMSDeal(json_message,messages,message,bmActivity)
+{
+    if(firstBMSMS(json_message,messages,message) == false)
+    {
+        NOFirstBMSMS(json_message,messages,message,bmActivity);
+    }
+}
+//不是第一个报名的信息，需要做是否重复处理
+function NOFirstBMSMS(json_message,messages,message,bmActivity)
+{
+    var isExist = false;
+    if(_.find(messages,function(message){
+        return message.phone == json_message.messages[0].phone && message.activity == bmActivity[0].name;
+    }))
+    {
+        isExist = true;
+    }
+    BMrepeat(json_message,message,isExist);
+}
+//报名尚未开始
+function NOActivityStart(json_message,bmActivity)
+{
+    if(bmActivity.length == 0)
+    {
+        native_accessor.send_sms(json_message.messages[0].phone,'活动尚未开始！');
+        console.log('活动尚未开始！');
+    }
+}
+//报名已结束
+function ActivityHasEnd(json_message,bmActivity)
+{
+    if(bmActivity.length != 0 && bmActivity[0].isStart == false)
+    {
+        native_accessor.send_sms(json_message.messages[0].phone,'活动已经结束！');
+        console.log('活动已经结束！');
+    }
+}
+//重复报名
+function BMrepeat(json_message,message,isExist)
+{
+    if(insert_sms_return_sms(json_message,message,isExist) == false)
+    {
+        native_accessor.send_sms(json_message.messages[0].phone,'已经报过名！');
+        console.log('已经报过名！');
+    }
 }
 
 //数据库里没有短信时
